2561250c570dd10d4b681b2335985b30587533d0
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-08 00:21:21 -0800

    add code for problem 1

--- main.js
+++ main.js
@@ -4,3 +4,15 @@
 
 console.log("******************************************");
 console.log("Problem 1\n");
+
+// Problem 1 Code
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+
+// Predict the output
+
+// Actual Output
+console.log(randomCar)
+console.log(otherRandomCar)
+
