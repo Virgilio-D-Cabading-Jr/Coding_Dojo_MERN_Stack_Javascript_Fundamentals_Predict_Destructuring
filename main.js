1c3a1a3c9a67aef71d4dda3138ac3c9045b1a411
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-08 01:09:04 -0800

    add problem 5 prediction

--- main.js
+++ main.js
@@ -85,9 +85,24 @@ console.log("\n******************************************");
 console.log("Problem 5\n");
 
 // Problem 5 Code:
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
 
 // Predict the output
 console.log("::: Predicted Output :::")
+console.log("value")
+console.log("[1, 5, 1, 8, 3, 3]")
+console.log("0")
+console.log("syntax error: secondKey is undefined")
 
 // Actual Output
 console.log("\n::: Actual Output :::")
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
