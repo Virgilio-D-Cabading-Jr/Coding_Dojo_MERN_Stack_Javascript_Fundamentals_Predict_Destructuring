d01a97e0a081d82a2f23191be9894c0c3b2fa85a
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-08 00:58:04 -0800

    add problem 3 code and prediction

--- main.js
+++ main.js
@@ -64,3 +64,19 @@ console.log(hashedPassword);
 
 console.log("\n******************************************");
 console.log("Problem 4\n");
+
+// Problem 4 Code:
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+
+// Predict the output
+console.log("::: Predicted Output :::")
+console.log("5")
+console.log("2")
+
+// Actual Output
+console.log("\n::: Actual Output :::")
+console.log(first == second);
+console.log(first == third);
